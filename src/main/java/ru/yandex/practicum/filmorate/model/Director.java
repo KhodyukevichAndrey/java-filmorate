e7239e4c58f0867fbe8da3b66ad70348b7b1730c
Андрей Ходюкевич <Lolds456@yandex.ru>
2023-10-11 01:11:05 +0400

    add-director vers 1.0

--- src/main/java/ru/yandex/practicum/filmorate/model/Director.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Director.java
@@ -3,14 +3,14 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Data
 @AllArgsConstructor
 public class Director {
     private int id;
-    @NotNull
     @Size(max = 200)
+    @NotBlank
     private String name;
 }
